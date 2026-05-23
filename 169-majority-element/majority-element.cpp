6ac852e010407df33c905b87f9f68d331b3830f9
Kritika Bhanaria <145003406+kritika89898@users.noreply.github.com>
2026-05-23 18:12:03 +0530

    Time: 1 ms (34.11%) | Memory: 28.2 MB (27.17%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(auto x:nums){
+            mp[x]++;
+        }
+        for(auto p : mp){
+            if(p.second > nums.size()/2){
+                return p.first;
+            }
+        }
+        return -1;
+    }
+};
